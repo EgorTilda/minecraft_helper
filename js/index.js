3355cca5cf5a0d8e4b4acb7c63cec8c70129353f
Egor <egortilda@gmail.com>
2020-08-15 14:25:44 +0300

    tabs

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// tabs
+const tabsControl = document.querySelectorAll('.menu__item');
+
+const hideTabs = () => {
+  document.querySelectorAll('.tab').forEach( (item) => {
+    item.classList.add('hide');
+  });
+};
+
+tabsControl.forEach( (item) => {
+  item.addEventListener('click', (e) => {
+    if(e.target.tagName == 'IMG') {
+     hideTabs();
+     document.querySelector(`.${e.target.id}`).classList.remove('hide');
+     document.querySelector(`.${e.target.id}`).classList.add('active');
+    }
+  });
+});
